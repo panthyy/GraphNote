eeeb8d25e7dcdcee97d9586d50912fe048ba02bc
panthyy <fahad.alhatshami@hotmail.com>
2022-09-21 02:49:43 +0200

    add: generated code

--- src/server/router/generated/node-router.ts
+++ src/server/router/generated/node-router.ts
@@ -1,74 +1,98 @@
-import { createRouter } from "../context";
-import { z } from "zod";
-import * as trpc from "@trpc/server";
 
-import { createProtectedRouter } from "../protected-router";
+import { createRouter } from '../context'
+import { z } from 'zod'
+import * as trpc from '@trpc/server'
 
-export const node = createRouter().merge(
+import { createProtectedRouter } from '../protected-router'
+
+export const node = createRouter()
+.merge(
   createProtectedRouter()
-    .query("get", {
-      input: z.object({
+  .query('get', {
+
+
+    input: z.object({
         id: z.string().nullish(),
-      }),
+
+    }),
       async resolve({ ctx, input }) {
-        const node = await ctx.prisma.node.findMany({
+        const node =  await ctx.prisma.node.findMany({
           where: {
             user_id: ctx.session.user.id,
-            id: input.id || undefined,
+              id: input.id || undefined ,
           },
           select: {
-            id: true,
-            title: true,
-            created_at: true,
-            updated_at: true,
-            data: true,
-            root_id: true,
-            user_id: true,
-          },
-        });
-        return node;
+              id: true,
+              title: true,
+              created_at: true,
+              updated_at: true,
+              data: true,
+              root_id: true,
+              user_id: true,
+          }
+        })
+        return node
+
+
+
       },
     })
-    .query("post", {
-      input: z.object({
+  .query('post', {
+
+
+    input: z.object({
+
         user_id: z.string(),
         title: z.string().nullish(),
         data: z.string().nullish(),
         root_id: z.string().nullish(),
-      }),
+        root: z.().nullish(),
+      
+    }),
       async resolve({ ctx, input }) {
-        const node = await ctx.prisma.node.create({
+
+       const node =  await ctx.prisma.node.create({
           data: {
-            title: input.title,
-            data: input.data,
-            root_id: input.root_id,
-            user_id: input.user_id,
+              title: input.title ,
+              data: input.data ,
+              root_id: input.root_id ,
+              user_id: input.user_id ,
+
           },
           select: {
-            id: true,
-            title: true,
-            created_at: true,
-            updated_at: true,
-            data: true,
-            root_id: true,
-            user_id: true,
-          },
-        });
-        return node;
+              id: true,
+              title: true,
+              created_at: true,
+              updated_at: true,
+              data: true,
+              root_id: true,
+              user_id: true,
+          }
+        })
+        return node
+
+
       },
     })
-    .query("delete", {
-      input: z.object({
+  .query('delete', {
+
+
+    input: z.object({
         id: z.string().nullish(),
-      }),
+
+    }),
       async resolve({ ctx, input }) {
-        const node = await ctx.prisma.node.deleteMany({
+
+
+      const node = await ctx.prisma.node.deleteMany({
           where: {
             user_id: ctx.session.user.id,
-            id: input.id || undefined,
-          },
-        });
-        return node.count;
+              id: input.id || undefined ,
+          }
+
+        })
+      return node.count
+
       },
     })
-);
+)
